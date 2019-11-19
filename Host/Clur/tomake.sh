c60c4de634999bb7b9e8be53b4383cd3e001f6fa
dvdagaev <54113249+dvdagaev@users.noreply.github.com>
2019-11-19 10:34:48 +0300

    Upgrades host system 2

---
+++ Host/Clur/tomake.sh
@@ -0,0 +1,4 @@
+../../Blur/llc -march=x86-64 -filetype=obj HostConLog.ll -o HostConLog.o
+../../Blur/llc -march=x86-64 -filetype=obj HostTimes.ll -o HostTimes.o
+../../Blur/llc -march=x86-64 -filetype=obj HostFiles.ll -o HostFiles.o
+
