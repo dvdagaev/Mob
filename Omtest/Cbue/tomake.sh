910731423a0189a081aff60f5f6d15ebcfcb1044
dvdagaev <dvdagaev@mail.ru>
2020-12-01 14:14:58 +0300

    Examples and tests version 1

---
+++ Omtest/Cbue/tomake.sh
@@ -0,0 +1,10 @@
+../../Bbue/ombsh co OmtestHelloWorld
+../../Bbue/ombsh co OmtestFormats
+../../Bbue/ombsh co OmtestDateTime
+../../Bbue/ombsh co OmtestMkTraps
+../../Bbue/ombsh co OmtestHeap
+../../Bbue/ombsh li -dos -o Omtest/Code/OmtestHelloWorld.exe $+Kernel Log Math Strings OStrings OLog HostConLog Runner OmtestHelloWorld
+../../Bbue/ombsh li -dos -o Omtest/Code/OmtestFormats.exe $+Kernel Log Math Strings OStrings OLog HostConLog Runner OmtestFormats
+../../Bbue/ombsh li -dos -o Omtest/Code/OmtestDateTime.exe $+Kernel Log Math Strings OStrings OLog Times HostTimes HostConLog Runner OmtestDateTime
+../../Bbue/ombsh li -dos -o Omtest/Code/OmtestMkTraps.exe $+Kernel Log Math Strings OStrings OLog HostConLog Runner OmtestMkTraps
+../../Bbue/ombsh li -dos -o Omtest/Code/OmtestHeap.exe $+Kernel Log Math Strings OStrings OLog HostConLog Runner OmtestHeap
